d17bf97956d07e3af9b6658378a50bd35c8ca147
mazilkhatib <mazilkhatib001@gmail.com>
2023-11-24 01:35:03 +0530

    footer and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Navbar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import MovieGrid from "@/components/MovieGrid"; // Import the MovieGrid component
-import moviesData from "@/public/movies.json"; // Import the movies data
+import moviesData from "@/public/movies.json";
+import Footer from "@/components/Footer"; // Import the movies data
 
 export default function Home() {
     return (
@@ -12,9 +13,10 @@ export default function Home() {
                 subtitle="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates."
                 image="https://upload.wikimedia.org/wikipedia/en/3/30/Ant-Man_and_the_Wasp_Quantumania_poster.jpg"
             />
-            <div className="lg:flex">
+            <div className="lg:flex mb-5">
                 <MovieGrid movies={moviesData} />
             </div>
+            <Footer/>
         </>
 
     )
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,13 +9,12 @@ export default function Navbar() {
           <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
               <a
-                  className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-                  href="#pablo"
+                  className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
               >
                 Movies App
               </a>
               <button
-                  className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                  className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                   type="button"
                   onClick={() => setNavbarOpen(!navbarOpen)}
               >
