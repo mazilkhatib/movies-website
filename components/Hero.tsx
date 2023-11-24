779f2a04276def8b0b82073a23ac2ac4092aaf3e
mazilkhatib <mazilkhatib001@gmail.com>
2023-11-24 16:25:18 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,15 @@
-import React,{FC} from 'react';
+import React, { FC } from 'react';
+
 interface HeroProps {
     title: string;
-    subtitle: string | undefined;
-    image: string | undefined;
+    subtitle: string;
+    image: string;
 }
 
 const Hero: FC<HeroProps> = ({ title, subtitle, image }) => (
-    <div className="lg:flex">
-        <div className="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2 lg:ml-16 lg:pl-16">
-            <div className="max-w-xl">
+    <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
+        <div className="w-full lg:w-1/2">
+            <div className="lg:max-w-lg lg:ml-20">
                 <h2 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">{title}</h2>
 
                 <p className="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">{subtitle}</p>
@@ -16,13 +17,17 @@ const Hero: FC<HeroProps> = ({ title, subtitle, image }) => (
                 <div className="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
                     <a href="#" className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700">Watch Now</a>
                 </div>
+
             </div>
+
         </div>
 
-        <div className="w-full h-64 lg:w-1/5 lg:h-auto lg:ml-56 lg:p-12 ">
-            <div className="w-full h-full bg-cover rounded-lg" style={{ backgroundImage: `url(${image})` }}>
-                <div className="w-full h-full bg-black opacity-25 "></div>
-            </div>
+        <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
+            <img
+                className="object-cover w-full h-full mx-auto rounded-md lg:w-1/2"
+                src={image}
+                alt="glasses photo"
+            />
         </div>
     </div>
 );
