67e3c79f65a901b97cc8c9efedb9fb7dd119aeae
mazilkhatib <mazilkhatib001@gmail.com>
2023-11-24 12:15:46 +0530

    Update MovieGrid.tsx

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
 
 interface MovieGridProps {
     movies: any[];
@@ -22,28 +23,9 @@ const MovieGrid: React.FC<MovieGridProps> = ({ movies }) => {
 
     const paginatedMovies = filteredMovies.slice(0, page * itemsPerPage);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        filterMovies(movies, searchTerm);
-    };
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(e.target.value);
-    };
-
     return (
         <div className="container mx-auto">
-            <form onSubmit={handleSubmit} className="flex justify-center my-4">
-                <div className="relative mt-4 md:mt-0">
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                        <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </span>
-                    <input type="text" onChange={handleChange} className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search"/>
-                </div>
-            </form>
-
+            <SearchBar setSearchTerm={setSearchTerm}/>
             <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-2">
                 {paginatedMovies.map((movie) => (
                     // eslint-disable-next-line react/jsx-key
