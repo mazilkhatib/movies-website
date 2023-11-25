84ee0063261cc22e465ff8cb6834f8858cff3298
mazilkhatib <mazilkhatib001@gmail.com>
2023-11-25 22:21:24 +0530

    data update

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,19 @@ import moviesData from "@/public/movies.json";
 import Footer from "@/components/Footer";
 
 export default function Home() {
+    let filteredMovies = [];
+
+    for (let movie of moviesData) {
+        if (movie.thumbnail) {
+            movie.href = movie.href.replace(/%/g, "_");
+            filteredMovies.push(movie);
+        }
+    }
+
     return (
         <>
             <Navbar/>
-            <Hero moviesData={moviesData} />
+            <Hero moviesData={filteredMovies} />
             <div className="lg:flex mb-5">
                 <MovieGrid movies={moviesData} />
             </div>
